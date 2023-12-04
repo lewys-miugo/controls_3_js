47f95e581aaf888a8ded303c384c8f8ef32d873e
lewys-miugo <lewiswambugu01@gmail.com>
2023-12-04 16:16:35 +0300

    Initial File uploads

--- main.js
+++ main.js
@@ -1,24 +1,47 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+// Include Three.js and OrbitControls
+import * as THREE from 'three';
+import "./style.css";
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls"
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// Set up your scene, camera, and renderer
 
-setupCounter(document.querySelector('#counter'))
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Create a cube
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+// Set up OrbitControls
+const controls = new OrbitControls(camera, renderer.domElement);
+
+// Set initial camera position
+camera.position.z = 5;
+
+// Animation loop
+const animate = function () {
+  requestAnimationFrame(animate);
+
+  // Update controls
+  controls.update();
+
+  renderer.render(scene, camera);
+};
+
+// Handle window resize
+window.addEventListener('resize', function () {
+  const newWidth = window.innerWidth;
+  const newHeight = window.innerHeight;
+
+  camera.aspect = newWidth / newHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(newWidth, newHeight);
+});
+
+animate();
